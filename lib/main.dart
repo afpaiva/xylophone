1326ac1176461b316f52e49d470d10c03816457e
afpaiva <andrepaiva.arq@gmail.com>
2021-12-20 22:45:43 -0300

    done

--- lib/main.dart
+++ lib/main.dart
@@ -10,18 +10,40 @@ class XylophoneApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final audioPlayer = AudioPlayer();
+    final player = AudioCache(fixedPlayer: audioPlayer);
+
+    Container keys(Color color, int note) {
+      return Container(
+        width: double.infinity,
+        color: color,
+        child: TextButton(
+          onPressed: () {
+            audioPlayer.stop();
+            player.play('note$note.wav');
+          },
+          child: const Text(''),
+        ),
+      );
+    }
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-              child: TextButton(
-            onPressed: () {
-              final player = AudioCache();
-              player.play('assets_note1.wav');
-            },
-            child: Text('Click me'),
-          )),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(child: keys(Colors.red, 1)),
+              Expanded(child: keys(Colors.orange, 2)),
+              Expanded(child: keys(Colors.yellow, 3)),
+              Expanded(child: keys(Colors.green, 4)),
+              Expanded(child: keys(Colors.teal, 5)),
+              Expanded(child: keys(Colors.blue, 6)),
+              Expanded(child: keys(Colors.purple, 7)),
+            ],
+          ),
         ),
       ),
     );
